bb06c8c51e83120478d900af13bcadbfb59711b9
alangamu <alangamu@gmail.com>
2024-03-24 01:20:41 -0300

    bigger buttons

--- Assets/Scripts/Startup.cs
+++ Assets/Scripts/Startup.cs
@@ -1,4 +1,5 @@
-﻿using Assets.Scripts.ScriptableObjects.Variables;
+﻿using Assets.Scripts.ScriptableObjects.Events;
+using Assets.Scripts.ScriptableObjects.Variables;
 using UnityEngine;
 
 namespace Assets.Scripts
@@ -9,6 +10,30 @@ namespace Assets.Scripts
         private LobbyManager _lobbyManager;
         [SerializeField]
         private BoolVariable _isStartupLoaded;
+        [SerializeField]
+        private GameEvent _quitGameEvent;
+
+        private void Awake()
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+
+        private void OnEnable()
+        {
+            _quitGameEvent.OnRaise += QuitGame;
+        }
+
+        private void OnDisable()
+        {
+            _quitGameEvent.OnRaise -= QuitGame;
+        }
+
+        private void QuitGame()
+        {
+            AndroidJavaObject activity = new AndroidJavaClass("com.unity3d.player.UnityPlayer").GetStatic<AndroidJavaObject>("currentActivity");
+            activity.Call<bool>("moveTaskToBack", true);
+            //Application.Quit();
+        }
 
         private async void Start()
         {
